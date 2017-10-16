93deec49808690c2e0c95ff0afcbabe1ac919d62
hmelo1 <heriberto@melo.nyc>
2017-10-16 02:19:27 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -10,7 +10,11 @@ function frameIt(){
   })
 }
 function submitIt(){
-
+  $("form").on("submit", function(){
+    if ($( "input:first").val() === "correct"){
+      alert('Your form is going to be submitted now.')
+    }
+  })
 }
 $(document).ready(function(){
 
