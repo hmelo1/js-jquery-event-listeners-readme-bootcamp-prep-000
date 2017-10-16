af0996e0730b13f0174e7537e4dddd757d80c8fd
hmelo1 <heriberto@melo.nyc>
2017-10-16 02:21:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,6 +13,7 @@ function submitIt(){
   $("form").on("submit", function(){
     if ($( "input:first").val() === "correct"){
       alert('Your form is going to be submitted now.')
+      return
     }
   })
 }
