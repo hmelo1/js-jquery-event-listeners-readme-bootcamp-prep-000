9b8a6db1516c3f65588f13327e14e3ed2fd63e05
hmelo1 <heriberto@melo.nyc>
2017-10-16 02:11:52 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -4,9 +4,14 @@ function getIt(){
     alert("Hey!")
   })
 }
-
+function frameIt(){
+  $('img').on('load', function(){
+    $(this).addClass('tasty')
+  })
+}
 $(document).ready(function(){
 
 // call functions here
   getIt()
+  frameIt()
 });
